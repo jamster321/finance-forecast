24dd74fe2253bb2116a8ab9f91a667d3881de04b
Jamie Willis <jamster321@gmail.com>
2020-09-23 22:19:30 +0100

    Fix so that the transaction form correctly displays the edited transaction the first time it is opened
     This is a bit of a hack.
     self.editedTransaction doesn't immediately change when the TransactionRow onEdit func sets it,
     because self.editedTransaction isn't initially used anywhere in the UI.
     It is used by TransactionForm, however TransactionForm hasn't been rendered by the UI until it has been
     displayed at least once, and so it doesn't start working until the second time the sheet is displayed.
     To get around this I've added this Text view (in a ZStack, so it's not visible), which uses self.editedTransaction.
     The answer may be to add binding to the TransactionForm transaction or to initialise TransactionForm.

--- finance-forecast/TransactionList.swift
+++ finance-forecast/TransactionList.swift
@@ -23,27 +23,38 @@ struct TransactionList: View {
     }
     
     var body: some View {
-        VStack {
-            List {
-                ForEach(forecast.transactions, id: \.self) { transaction in
-                    if (!self.hideComplete || !transaction.complete) {
-                        TransactionRow(
-                            transaction: transaction,
-                            onEdit: {
-                                self.editedTransaction = transaction
-                                self.isPresented = true
-                            }
-                        )
+        ZStack {
+            VStack {
+                List {
+                    ForEach(forecast.transactions, id: \.self) { transaction in
+                        if (!self.hideComplete || !transaction.complete) {
+                            TransactionRow(
+                                transaction: transaction,
+                                onEdit: {
+                                    self.editedTransaction = transaction
+                                    self.isPresented = true
+                                }
+                            )
+                        }
                     }
+                    .onDelete(perform: deleteTransaction)
                 }
-                .onDelete(perform: deleteTransaction)
+                .listStyle(PlainListStyle())
+                .sheet(isPresented: $isPresented, onDismiss: {
+                    self.editedTransaction = nil
+                }) {
+                    TransactionForm(forecast: self.forecast, transaction: self.editedTransaction, moc: self.moc)
+                }
             }
-            .listStyle(PlainListStyle())
-            .sheet(isPresented: $isPresented, onDismiss: {
-                self.editedTransaction = nil
-            }) {
-                TransactionForm(forecast: self.forecast, transaction: self.editedTransaction, moc: self.moc)
-            }
+            
+            // This is a bit of a hack.
+            // self.editedTransaction doesn't immediately change when the TransactionRow onEdit func sets it,
+            // because self.editedTransaction isn't initially used anywhere in the UI.
+            // It is used by TransactionForm, however TransactionForm hasn't been rendered by the UI until it has been
+            // displayed at least once, and so it doesn't start working until the second time the sheet is displayed.
+            // To get around this I've added this Text view (in a ZStack, so it's not visible), which uses self.editedTransaction.
+            // The answer may be to add binding to the TransactionForm transaction or to initialise TransactionForm.
+            Text(self.editedTransaction != nil ? "" : "").hidden()
         }
         .navigationBarTitle(Text(self.forecast.monthString()), displayMode: .inline)
         .navigationBarItems(trailing:
